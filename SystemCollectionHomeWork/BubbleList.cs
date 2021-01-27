52101cb768683d870075c127b980f69bb0288547
Bolsyn <Serikagis@yandex.ru>
2021-01-27 11:06:45 +0600

    Add project files.

---
+++ SystemCollectionHomeWork/BubbleList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SystemCollectionHomeWork
+{
+    class BubbleList<T> : List<T>
+    {
+        private class Node
+        {
+            public Node Next;
+            public int Data;
+        }
+
+        private Node head = null;
+
+        public int Size()
+        {
+            int listsize = 0;
+            Node current = head;
+            while (current != null)
+            {
+                listsize++;
+                current = current.Next;
+            }
+            return listsize;
+        }
+
+        public void OrderListBubbleAlgoritm()
+        {
+            Node current = head;
+            for (int i = 1; i < Size()-1; i++)
+            {
+                for (int j = 0; j < Size() - 1; j++)
+                {
+                    if (current.Data > current.Next.Data && current.Next != null)
+                    {
+                        int temp = current.Data;
+                        current.Data = current.Next.Data;
+                        current.Next.Data = temp;
+                    }
+
+                }
+            }
+            head = current.Next;           
+        }
+    }
+}
---
+++ SystemCollectionHomeWork/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SystemCollectionHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            var list = new List<int>(10);
+            int count = 10;
+            for (int i =0; i < count; i++)
+            {
+                list.Add(rand.Next() % 90 + 10);
+            }
+            int max=0;
+            int max2=0;
+
+            foreach (var el in list)
+            {
+                Console.WriteLine(el);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < count; i++)
+            {
+                if (list[i] > max)
+                {
+                    max = list[i];
+                }
+            }
+            for (int i = 0; i < count; i++)
+            {
+                if (list[i] < max && list[i] > max2)
+                {
+                    max2 = list[i];
+                }
+            }
+
+            Console.WriteLine(max2);
+
+            int sum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (i%2==0)
+                {
+                    sum += list[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
